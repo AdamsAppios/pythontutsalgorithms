c32d8f69357f0c4a59b58cee7ca7cf539009caf4
AdamsAppios <soadam390@gmail.com>
2023-07-25 03:04:12 +0800

    testing commit

--- Graph Algorithms/dijkstras.py
+++ Graph Algorithms/dijkstras.py
@@ -1,37 +1,37 @@
 import heapq
 
-def dijkstra(graph, start):
-    distances = {node: float('inf') for node in graph}  # Initialize distances to infinity
-    distances[start] = 0  # Distance from start node to itself is 0
-    queue = [(0, start)]  # Priority queue to keep track of nodes to visit
+def dijkstra(graph, source):
+    # Initialize distances and priority queue
+    distances = {vertex: float('inf') for vertex in graph}
+    distances[source] = 0
+    priority_queue = [(0, source)]
 
-    while queue:
-        current_distance, current_node = heapq.heappop(queue)  # Get node with smallest distance
-        if current_distance > distances[current_node]:
-            continue  # Skip if we've already found a shorter path
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
 
-        for neighbor, weight in graph[current_node].items():
+        # Skip if the current distance is greater than the recorded distance
+        if current_distance > distances[current_vertex]:
+            continue
+
+        # Update distances for neighbors of the current vertex
+        for neighbor, weight in graph[current_vertex].items():
             distance = current_distance + weight
-            if distance < distances[neighbor]:  # Found a shorter path
+            if distance < distances[neighbor]:
                 distances[neighbor] = distance
-                heapq.heappush(queue, (distance, neighbor))
+                heapq.heappush(priority_queue, (distance, neighbor))
 
     return distances
 
-
-# Example usage
+# Example graph represented as an adjacency list
 graph = {
-    'A': {'B': 5, 'C': 2},
-    'B': {'D': 4, 'E': 2},
-    'C': {'B': 8, 'E': 7},
-    'D': {'E': 6, 'F': 3},
-    'E': {'F': 1},
-    'F': {}
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
 }
 
-start_node = 'A'
-distances = dijkstra(graph, start_node)
+source_vertex = 'A'
+shortest_distances = dijkstra(graph, source_vertex)
 
-print(f"Shortest distances from node {start_node}:")
-for node, distance in distances.items():
-    print(f"To node {node}: {distance}")
+print("Shortest distances from vertex", source_vertex, "to all other vertices:")
+print(shortest_distances)
